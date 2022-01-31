ed3ccc11b916e774fb4cc5534379122642d5d294
H2yOdes2a <andressainacia8@gmail.com>
2022-01-31 11:14:26 -0300

    update challenge

---
+++ script.js
@@ -0,0 +1,20 @@
+// Crie um array que receba 5 itens e exiba no console.
+let nomes=["hinata","dazai","ayato","mafuyu","ash"]
+console.log(nomes)
+// Utilize um método para adicionar um nome ao inicio do array.
+nomes.unshift("levi")
+console.log(nomes)
+// Utilize um método para remover o último nome do array.
+nomes.pop()
+console.log(nomes)
+// Utilize um método para adicionar dois nomes ao fim do array.
+nomes.push("mai","taiga")
+console.log(nomes)
+// Utilize um método para remover o primeiro nome do array.
+nomes.shift()
+console.log(nomes)
+// Utilize um método para organizar em ordem crescente o seguinte array:
+ const numbers = [7,5,6,3,8,9,2,1,4]
+ numbers.sort(function(a,b){
+	return a-b})
+    console.log(numbers)
